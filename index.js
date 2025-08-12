005f715cee7c5b2d9cde0e3f8a1d896dcc212d50
Caleb Adebayo <tolorunlekecaleb@gmail.com>
2025-08-12 12:53:44 -0700

    update so far

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 const PORT = 3002
 const { movies } = require('./db/movies')
 
+app.use(express.json())
+
 app.get('/api/v1/', (req, res) => {
     console.log('the home page');
     res.send('Home page')
@@ -13,44 +15,80 @@ app.get('/api/v1/', (req, res) => {
 // get all movies
 app.get('/api/v1/movies/', (req, res) => {
     console.log('all movies returned');
-    // console.log(movies.id)
-    // res.send('all movies')
     res.status(200).json({ movies })
 })
 
 //get a single movie
 app.get('/api/v1/movies/:singleId/', (req, res) => {
     const { singleId } = req.params
-    // const id = movies.id
     const movieId = parseInt(singleId)
+
     const getSingleMovie = movies.find(movie => movie.id === movieId)
+
     if (getSingleMovie) {
-        res.status.send(getSingleMovie.json)
-        return movieId
+        res.status(200).json(getSingleMovie)
     } else {
-        return console.log('invalid')
+        res.status(401).json({ msg: 'invalid request' })
 
     }
 })
 
 //create a movie
-app.get('/api/v1/movie', (req, res) => {
-    console.log('movie added successfully')
-    res.send('movie added successfully')
+app.post('/api/v1/movie', (req, res) => {
+    const newMovie = req.body
+    const { id } = req.body
+    const movieEcist = movies.find(movie => movie.id === id)
+
+    if (!newMovie || !newMovie.id || !newMovie.title) {
+        res.status(400).json({
+            msg: "kindly provide new movie data"
+        })
+    }
+    else if (movieEcist) {
+        res.status(400).json({
+            msg: "movie already exist"
+        })
+    }
+    else {
+        movies.push(newMovie)
+
+        res.status(201).json({
+            msg: "new movie inserted successfully",
+            movie: newMovie,
+            movies: movies.length
+        })
+    }
+
 })
 
 //delete a movie
-app.get('/api/v1/movie/delete/:movieId', (req, res) => {
+app.delete('/api/v1/movie/delete/:movieId', (req, res) => {
     const { movieId } = req.params
-    console.log(`movie ${movieId} deleted successfully`)
-    res.send(movieId)
+    const singleMovie = parseInt(movieId)
+
+    const remainingMovie = movies.filter(movie => movie.id !== singleMovie)
+
+    res.status(201).json({
+        msg: "deleted successfully",
+        movies: remainingMovie
+    })
 })
 
+
+
 // update a movie 
-app.get('/api/v1/movie/update/:movieId', (req, res) => {
+app.patch('/api/v1/movie/update/:movieId', (req, res) => {
     const { movieId } = req.params
-    console.log(`movie ${movieId} updated successfully`)
-    res.send(movieId)
+    const updateData = req.body
+    const singleId = parseInt(movieId)
+
+    const movieIndex = movies.findIndex(movie => movie.id === singleId)
+    movies[movieIndex] = { ...movies[movieIndex], ...updateData }
+
+    res.status(201).json({
+        msg: "movie updated successfully",
+        movie: movies[movieIndex]
+    })
 })
 
 app.listen(PORT, () => {
