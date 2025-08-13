637ad665a445c326dc5d4317865c4d321bc5a430
Caleb Adebayo <calebtolorunleke@yahoo.com>
2025-08-13 10:19:20 -0700

    completed task

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const router = require('./routes/movie')
 app.use(express.json())
 app.use('/api/', router)
 
+// db connection 
+const dbURI = 'mongodb+srv://admin:password@1@cluster0.rylvtbj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
 
 
 app.listen(PORT, () => {
